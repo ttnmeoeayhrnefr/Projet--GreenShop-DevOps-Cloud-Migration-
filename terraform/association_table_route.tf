43cc0af63e669dbc77b14c9e90239da43b5a377e
ttnmeoeayhrnefr <145941540+ttnmeoeayhrnefr@users.noreply.github.com>
2025-05-07 11:23:43 +0200

    Update association_table_route.tf

--- terraform/association_table_route.tf
+++ terraform/association_table_route.tf
@@ -1,14 +1,14 @@
-resource "aws_route_table_association" "Ben-privrtb-assoc1" {
-  route_table_id = aws_route_table.Ben-privrtb.id
-  subnet_id      = aws_subnet.Ben-priv1.id
+resource "aws_route_table_association" "GreenShop-privrtb-assoc1" {
+  route_table_id = aws_route_table.GreenShop-privrtb.id
+  subnet_id      = aws_subnet.GreenShop-priv1.id
 }
 
-resource "aws_route_table_association" "Ben-privrtb-assoc2" {
-  route_table_id = aws_route_table.Ben-privrtb.id
-  subnet_id      = aws_subnet.Ben-priv2.id
+resource "aws_route_table_association" "GreenShop-privrtb-assoc2" {
+  route_table_id = aws_route_table.GreenShop-privrtb.id
+  subnet_id      = aws_subnet.GreenShop-priv2.id
 }
 
-resource "aws_route_table_association" "Ben-privrtb-assoc3" {
-  route_table_id = aws_route_table.Ben-privrtb.id
-  subnet_id      = aws_subnet.Ben-priv3.id
+resource "aws_route_table_association" "GreenShop-privrtb-assoc3" {
+  route_table_id = aws_route_table.GreenShop-privrtb.id
+  subnet_id      = aws_subnet.GreenShop-priv3.id
 }
