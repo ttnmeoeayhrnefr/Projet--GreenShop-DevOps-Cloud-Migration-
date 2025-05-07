e5eb3122324cd02d0ae098f0596796c6e7db7f2f
Théo MENANT--FERRY <t.menantferry@ecole-ipssi.net>
2025-05-07 16:20:20 +0200

    use variables for ssh to bastion authorized ips

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "GreenShop-SG-ADM" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.allowed_ssh_ips
   }
 
   egress {
