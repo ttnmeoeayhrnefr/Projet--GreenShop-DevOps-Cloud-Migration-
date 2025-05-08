fb8812b515c7ee5693c03e625a5817bf059185b5
Théo MENANT--FERRY <t.menantferry@ecole-ipssi.net>
2025-05-08 14:43:37 +0200

    auo update ansible inventory with aws public ips

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
+    }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
+provider "local" {}
